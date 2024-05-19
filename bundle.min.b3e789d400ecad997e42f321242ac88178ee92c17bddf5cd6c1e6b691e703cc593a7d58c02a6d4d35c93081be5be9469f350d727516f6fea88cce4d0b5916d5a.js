d4d46200a2f308c2c66295535d3dc398fb486349
Joakim Skog <joakim.s@bokio.se>
2024-05-19 20:03:40 +0200

    Delete old code

--- bundle.min.b3e789d400ecad997e42f321242ac88178ee92c17bddf5cd6c1e6b691e703cc593a7d58c02a6d4d35c93081be5be9469f350d727516f6fea88cce4d0b5916d5a.js
+++
@@ -1,2 +0,0 @@
-document.addEventListener('DOMContentLoaded',function(event){var flag=0;let menuToggle=document.querySelector('.menu-button');let menu=document.querySelector('.menu');menuToggle.addEventListener('click',function(event){event.preventDefault();if(flag==0){menuToggle.classList.add('active');menu.classList.add('visible');flag=1;}
-else{menuToggle.classList.remove('active');menu.classList.remove('visible');flag=0;}});});
